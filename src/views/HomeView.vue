f63f5e4fd09c75301ece9d0356c43b02a89d53ef
chrelyonly <1172576293@qq.com>
2023-10-27 15:29:34 +0800

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,14 @@
 <script>
+export default {
+  data(){
+    return{
+      searchValue: "",
+    }
+  },
+  methods: {
+
+  }
+}
 </script>
 <style scoped>
 .el-row{
@@ -7,25 +17,92 @@
 </style>
 <template>
     <basic-container>
-      <el-row class="el-row">
+      <!--          顶部head-->
+      <el-row class="el-row" style="height: 10vh;overflow:hidden;">
         <el-col :span="24">
-          <el-tag>
-            纯绿色,无任何添加剂,请放心食用
-          </el-tag>
+          <el-row class="el-row">
+            <el-col :span="24">
+              <el-tag>
+                纯绿色,无任何添加剂,请放心食用
+              </el-tag>
+            </el-col>
+          </el-row>
+          <el-row class="el-row">
+            <el-col :span="24">
+              <el-button type="success" @click="$router.push('file-upload')">文件上传
+              </el-button>
+              <el-button type="success" @click="$router.push('music')">
+                音乐播放器
+              </el-button>
+            </el-col>
+          </el-row>
         </el-col>
       </el-row>
-      <el-row class="el-row">
-        <el-col :span="24">
-            <el-button type="success" @click="$router.push('file-upload')">文件上传
-            </el-button>
+<!--body-->
+      <el-row class="el-row" style="height: 75vh">
+        <el-col :span="3" class="left">
+<!--          left-menu-->
+          <el-row class="el-row">
+            <el-col :span="24">
+               <el-button class="left-menu" type="primary">AIGG</el-button>
+            </el-col>
+            <el-col :span="24">
+               <el-button class="left-menu" type="primary">搜索引擎</el-button>
+            </el-col>
+          </el-row>
         </el-col>
-      </el-row>
-      <el-row class="el-row">
-        <el-col :span="24">
-            <el-button type="success" @click="$router.push('music')">
-              音乐播放器
-            </el-button>
+        <el-col :span="21">
+          <!--          right body-->
+          <el-row class="el-row">
+            <el-col :span="8">
+            </el-col>
+            <el-col :span="8">
+              <el-input v-model="searchValue" :rows="5" placeholder="输入关键词"></el-input>
+            </el-col>
+            <el-col :span="8">
+            </el-col>
+          </el-row>
+          <el-row class="el-row" style="height: 100%;width: 100%;padding: 10px">
+            <el-col :span="24" style="background-color:#F2F4F5;;border-radius: 10px;overflow:hidden;;">
+              <el-row class="el-row" :gutter="10">
+                <el-col :span="24">
+                  <el-tag>热门搜索</el-tag>
+                </el-col>
+              </el-row>
+              <el-row class="el-row" :gutter="10">
+                <el-col :span="4">
+                  <div style="height:100px;width: 100%;background-color:red;border-radius: 10px"></div>
+                </el-col>
+                <el-col :span="4">
+                  <div style="height:100px;width: 100%;background-color:red;border-radius: 10px"></div>
+                </el-col>
+                <el-col :span="4">
+                  <div style="height:100px;width: 100%;background-color:red;border-radius: 10px"></div>
+                </el-col>
+                <el-col :span="4">
+                  <div style="height:100px;width: 100%;background-color:red;border-radius: 10px"></div>
+                </el-col>
+                <el-col :span="4">
+                  <div style="height:100px;width: 100%;background-color:red;border-radius: 10px"></div>
+                </el-col>
+                <el-col :span="4">
+                  <div style="height:100px;width: 100%;background-color:red;border-radius: 10px"></div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
         </el-col>
       </el-row>
     </basic-container>
 </template>
+<style scoped>
+.left{
+  overflow:hidden;
+  border-right: 1px solid #d7d6d6;
+  text-align: center;
+}
+.left-menu{
+  margin: 1px;
+  width: 90%;
+}
+</style>
